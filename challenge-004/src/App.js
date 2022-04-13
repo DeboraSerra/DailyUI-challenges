3f4f92ed71acfd2870ac3176bbdf4b9df3355e5a
Débora Serra <debora.r.serra@gmail.com>
2022-04-13 12:25:57 -0300

    Finish challenge 004

--- challenge-004/src/App.js
+++ challenge-004/src/App.js
@@ -13,11 +13,11 @@ box-sizing: border-box;
 const Visor = styled.section`
   box-sizing: border-box;
   width: 90%;
-  margin: auto;
-  height: 28px;
+  margin: 12px auto;
+  height: 48px;
   border: 1px solid black;
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-around;
   align-items: center;
   padding: 12px;
 `
@@ -25,7 +25,7 @@ const Visor = styled.section`
 const Pad = styled.section`
   box-sizing: border-box;
   width: 90%;
-  margin: auto;
+  margin: 12px auto;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -36,34 +36,129 @@ const Numbers = styled.button`
   width: 32px;
   height: 32px;
   border-radius: 8px;
+  margin: 8px;
 `
 
 function App() {
   const [state, setState] = useState({
-    nums: [],
+    num1: '',
+    num2: '',
+    operator: '',
+    result: '',
   })
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const operators = ['+', '-', '/', '*'];
 
-  const setOperation = ({ target }) => {
-    const num = parseFloat(target.id);
-    const lastNum = state.nums[state.nums.length - 1];
-    let newNum = num;
-    if (typeof num === 'number' && lastNum) {
-      newNum = parseFloat(`${lastNum}${num}`);
+  const setOperation = ({ target: { id } }) => {
+    const { num1, num2, operator, result } = state;
+    if (!operator && !operators.includes(id)) {
+      setState({ ...state, result: '', num1: num1 ? num1 + id : id });
+    } if (operators.includes(id)) {
+      if (result) {
+        setState({ ...state, result: '', num1: result, operator: id });
+      } else setState({ ...state, operator: id});
+    } if (operator && !operators.includes(id)) {
+      setState({ ...state, num2: num2 ? num2 + id : id });
+    } if (id === '=') {
+      switch (operator) {
+      case '+':
+        setState({ num1: '',
+          num2: '',
+          operator: '',
+          result: parseFloat(num1) + parseFloat(num2) });
+        break;
+      case '-':
+        setState({ num1: '',
+          num2: '',
+          operator: '',
+          result: parseFloat(num1) - parseFloat(num2) });
+        break;
+      case '/':
+        setState({ num1: '',
+          num2: '',
+          operator: '',
+          result: parseFloat(num1) / parseFloat(num2) });
+        break;
+      case '*':
+        setState({ num1: '',
+          num2: '',
+          operator: '',
+          result: parseFloat(num1) * parseFloat(num2) });
+        break;
+      default: return null;
+      }
     }
-    setState({ ...state, nums: [...state.nums, newNum]})
-    console.log(state);
   }
 
   return (
     <Wrapper>
-      <Visor>{0}</Visor>
+      <Visor>
+        <input
+          type="number"
+          name="num1"
+          value={ state.num1 }
+          style={{
+            width: '32px',
+            padding: '8px',
+            fontSize: '1.1rem',
+          }}
+          readOnly
+        />
+        <input
+          type="text"
+          name="operator"
+          value={ state.operator }
+          style={{
+            width: '32px',
+            padding: '8px',
+            fontSize: '1.1rem',
+          }}
+          readOnly
+        />
+        <input
+          type="number"
+          name="num2"
+          value={ state.num2 }
+          style={{
+            width: '32px',
+            padding: '8px',
+            fontSize: '1.1rem',
+          }}
+          readOnly
+        />
+        <input
+          type="text"
+          value={ state.result }
+          style={{
+            width: '32px',
+            padding: '8px',
+            fontSize: '1.1rem',
+          }}
+          readOnly
+        />
+      </Visor>
       <Pad>
         {numbers.map((num) => (
-          <Numbers key={num} id={num} onClick={ setOperation }>
+          <Numbers
+            key={num}
+            id={num}
+            onClick={ setOperation }
+          >
             {num}
           </Numbers>
         ))}
+      </Pad>
+      <Pad>
+        {operators.map((op) => (
+          <Numbers
+            key={op}
+            id={op}
+            onClick={ setOperation }
+          >
+            {op}
+          </Numbers>
+        ))}
+        <Numbers id="=" onClick={ setOperation }>=</Numbers>
       </Pad>
     </Wrapper>
   );
